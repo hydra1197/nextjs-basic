7b8695f32af5073ea0f5405ad488ade360dcb4a0
Khoi.nv <khoicoder.dn@gmail.com>
2020-02-12 10:29:59 +0700

    fix meta tage

--- components/Meta.js
+++ components/Meta.js
@@ -5,12 +5,14 @@ const Meta = ({ title, description, image }) => {
   return (
     <Head>
       <title>{ title }</title>
-      <meta name="og:title" property="og:title" content={title} />
-      <meta name="description" property="og:description" content={description} />
-      <meta name="og:description" property="og:description" content={description} />
+
+      <meta name="description" content={description} />
       <meta name="keywords" content="" />
-      <meta name="og:type" property="og:type" content="article" />
-      <meta name="og:image" property="og:image" content={image} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="article" />
+      <meta property="og:image" content={image} />
     </Head>
   )
 };
